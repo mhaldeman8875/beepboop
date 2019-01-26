7f3934871e3ed56d7be389b346c33466375ca892
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-01-25 19:39:57 -0800

    deleted specs comment in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,3 @@
-// Specs in ascending order of priority:
-//     * if the number inputted is divisible by 3, the program will return an error response using their name;
-//         - input: 30
-//         - output: Sorry, [name] but due to technical difficulties your input could not be computed
-    
-//     * the program will replace all digits in any number containing a 1 with "Boop";
-//         -input example: 12
-//         - output: boop, 2, [text], 4...Boop, boop, [text]
-    
-//     * the program will replace all digits in any number containing a zero with "Beep";
-//         - input example: 10
-//         - output example: beep, boop, 2, [text], 4, 5, [text], 7, 8, [text], beep
-
-
 //Business Logic:
 var bleepBloop = function(number) {
     
